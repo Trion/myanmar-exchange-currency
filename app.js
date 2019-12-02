5e61659b4c85c0157bd7249d350fec487d5bfc49
Nyein Chan Ko <nove0943@gmail.com>
2019-12-02 20:37:12 +0630

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+
+
+var proxyUrl = 'https://cors-anywhere.herokuapp.com/',
+    cbm_currency_url_ = 'http://forex.cbm.gov.mm/api/latest'
+var cbm_info_ ;
+fetch(proxyUrl + cbm_currency_url_)
+  .then(blob => blob.json())
+  .then(data => {
+    //console.table(data);
+    var json_data_ = JSON.stringify(data);
+   // console.log(json_data_);
+    var json_prase_ = JSON.parse(json_data_);
+    //console.log(json_prase_);
+    var json_parse_data_info_ = json_prase_.info;
+    //console.log(json_parse_data_info_);
+    return data;
+  })
+  . then (function (data){
+    var currency = Object.values(data.rates);
+    var currency_name_ = Object.getOwnPropertyNames(data.rates);
+    var cbm_info_name_ = data.info;
+    document.getElementById("info_").innerHTML = cbm_info_name_ +"<br>"+ Date();
+
+
+  for (var i = 0; i <currency.length; i++) {
+
+        var li = document.createElement("li");
+            li.className = "list-group-item bg-dark d-flex justify-content-between align-items-center";
+        var span = document.createElement("span");
+            span.className = "badge badge-primary badge-pill shadow";
+        var rate_change_name_ = document.createTextNode(currency_name_[i]);
+        var rate_change_ = document.createTextNode(currency[i]+ " MMK");
+        li.appendChild(rate_change_name_);
+            span.appendChild(rate_change_);
+            li.appendChild(span);
+        
+        
+        document.getElementById("cbm").appendChild(li);
+  }
+    
+
+  })
+  .catch(e => {
+    console.log(e);
+    return e;
+  });
+
